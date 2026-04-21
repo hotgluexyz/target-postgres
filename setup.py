10821cbd384cdfcba05fa666918a1bab88ffcf01
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2026-04-21 12:11:39 -0300

    Merge pull request #7 from hotgluexyz/HGI-4773-v2
    feat(HG-4773): implement `target-postgres` v2

--- setup.py
+++ setup.py
@@ -9,32 +9,36 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='singer-target-postgres',
-    url='https://github.com/datamill-co/target-postgres',
-    author='datamill',
-    version="0.2.5",
+    name='hotglue-target-postgres',
+    url='https://github.com/hotgluexyz/target-postgres',
+    author='hotglue',
+    version="1.0.0",
     description='Singer.io target for loading data into postgres',
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['target_postgres'],
     install_requires=[
-        'arrow==0.15.5',
-        'psycopg2-binary==2.9.3',
-        'singer-python==5.9.0'
-    ],
-    setup_requires=[
-        "pytest-runner"
+        'psycopg2-binary==2.9.11',
+        'joblib==1.5.2',
+        'ujson==5.11.0',
+        'fastjsonschema==2.21.2',
+        'singer-python==6.3.0',
+        'inflection==0.5.1'
     ],
     extras_require={
         'tests': [
-            "chance==0.110",
-            "Faker==4.0.3",
-            "pytest==5.4.1"
+            "pytest==9.0.1"
         ]},
     entry_points='''
       [console_scripts]
-      target-postgres=target_postgres:cli
+      target-postgres-beta=target_postgres:main
+      target-postgres=target_postgres:main
     ''',
-    packages=find_packages()
+    packages=find_packages(),
+    package_data = {
+        'target_postgres': [
+            'logging.conf'
+            ]
+    },
 )
